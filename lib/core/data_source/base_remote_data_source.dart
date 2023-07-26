2b9bfc4bd50e83ba4b57f0cec6398fba5a0de96b
Ahmed Shehatah <ahmed2002.eg@gmail.com>
2023-07-26 18:54:59 +0300

    feat: Add base remote data source

---
+++ lib/core/data_source/base_remote_data_source.dart
@@ -0,0 +1,40 @@
+import 'package:dio/dio.dart';
+import '../../../core/results/result.dart';
+import '../di/di_manager.dart';
+import '../enums/http_methods.dart';
+import '../net/api_provider.dart';
+import '../shared_prefs/shared_prefs.dart';
+
+abstract class RemoteDataSource {
+  static Future<Result<MODEL>> request<MODEL>(
+      {MODEL Function(dynamic)? converter,
+      MODEL Function(List<dynamic>?)? converterList,
+      required HttpMethod method,
+      required String url,
+      Map<String, dynamic>? data,
+      Map<String, dynamic>? headers,
+      Map<String, dynamic>? queryParameters,
+      bool listRequest = false,
+      FormData? formData,
+      bool? dataOnly,
+      bool? reqiureToken}) async {
+    headers ??= {};
+    headers['Content-Type'] = "application/json";
+    if (reqiureToken == true) {
+      headers['Authorization'] =
+          "Bearer ${DIManager.findDep<SharedPrefs>().getToken()}";
+    }
+
+    return await ApiProvider.sendObjectRequest<MODEL>(
+      converter: converter,
+      converterList: converterList,
+      method: method,
+      url: url,
+      data: data,
+      headers: headers,
+      dataOnly: dataOnly,
+      queryParameters: queryParameters,
+      formData: formData,
+    );
+  }
+}
