bd995646a6585a65fe3f21015518101c93c847fb
Ahmed Shehatah <ahmed2002.eg@gmail.com>
2023-07-26 18:05:29 +0300

    feat: setup netowrk module

---
+++ lib/data/remote/endpoints.dart
@@ -0,0 +1,17 @@
+class AppEndpoints {
+  // TODO fill set value to baseUrl
+  static const baseUrl = "";
+
+  // receiveTimeout
+  static const Duration receiveTimeout = Duration(milliseconds: 15000);
+
+  // connectTimeout
+  static const Duration connectionTimeout = Duration(milliseconds: 30000);
+
+  // put your endpoints here
+
+  // ex
+  // static const String login = "/login";
+  // static const String publisher = "/publisher";
+  // static const String publisherNews = "$publisher/news";
+}
