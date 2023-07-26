5309d9ad71fa9c96f31a964898753cd9002d6fbc
Ahmed Shehatah <ahmed2002.eg@gmail.com>
2023-07-26 17:43:11 +0300

    feat: add app consts file

---
+++ lib/core/constants/app_consts.dart
@@ -0,0 +1,8 @@
+class AppConsts {
+  // this line to prevent making objects from this class
+  AppConsts._();
+
+  // all declared variables here should be  static const
+
+  static const String appName = "News Pulse";
+}
