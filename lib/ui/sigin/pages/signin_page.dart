e3ea7ccd1f74ec4c015bfe3a5f28adcf71c826b6
hussein <husseinpotter19@gmail.com>
2023-08-04 07:01:54 +0300

    feat: Finish Sign In UI

--- lib/ui/sigin/pages/signin_page.dart
+++ lib/ui/sigin/pages/signin_page.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:news_pulse/core/constants/app_fonts.dart';
 import 'package:news_pulse/core/di/di_manager.dart';
 import 'package:news_pulse/core/enums/text_field_icons.dart';
 import 'package:news_pulse/core/utils/screen_utlis/device_utils.dart';
+import 'package:news_pulse/ui/global_widgets/default_button.dart';
 import 'package:news_pulse/ui/global_widgets/text_field_widget.dart';
 
 import '../../../core/constants/dimens.dart';
@@ -17,33 +19,36 @@ class SignInPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(),
       backgroundColor: DIManager.findCC().primaryColor,
-      body: Container(
-        padding: Dimens.cardInternalPadding,
-        child: SingleChildScrollView(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(height: ScreenHelper.fromHeight(8)),
-              Image.asset(
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            SizedBox(height: ScreenHelper.fromHeight(8)),
+            Padding(
+              padding: Dimens.horizontalPadding1,
+              child: Image.asset(
                 "assets/images/logo_transparent.png",
                 fit: BoxFit.contain,
               ),
-              SizedBox(height: ScreenHelper.fromHeight(15)),
-              TextFieldWidget(
-                  fieldIcon: iconsList[TextFieldIcon.email]!,
-                  textController: emailController,
-                  label: "Email",
-                  hint: "Enter Email"),
-              TextFieldWidget(
-                fieldIcon: iconsList[TextFieldIcon.password]!,
-                textController: passwordController,
-                label: "Password",
-                hint: "Enter Password",
-                isPassword: true,
-              )
-            ],
-          ),
+            ),
+            SizedBox(height: ScreenHelper.fromHeight(10)),
+            TextFieldWidget(
+                fieldIcon: iconsList[TextFieldIcon.email]!,
+                textController: emailController,
+                label: "Email",
+                hint: "example@test.com"),
+            TextFieldWidget(
+              fieldIcon: iconsList[TextFieldIcon.password]!,
+              textController: passwordController,
+              label: "Password",
+              hint: "Enter Password",
+              isPassword: true,
+            ),
+            SizedBox(height: ScreenHelper.fromHeight(5)),
+            DefaultButton(buttonText: "Submit", onPressed: () {}),
+            SizedBox(height: ScreenHelper.fromHeight(5)),
+          ],
         ),
       ),
     );
