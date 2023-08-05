48d34459533185b1bd6b803237636c0d3b53c58d
hussein <husseinpotter19@gmail.com>
2023-08-05 15:26:36 +0300

    refactor: change publisher_model import

--- lib/data/sources/auth_remote_data_source.dart
+++ lib/data/sources/auth_remote_data_source.dart
@@ -1,9 +1,9 @@
 import 'package:news_pulse/core/data_source/base_remote_data_source.dart';
 import 'package:news_pulse/core/enums/http_methods.dart';
-import 'package:news_pulse/data/models/publisher_model.dart';
 import 'package:news_pulse/data/models/sign_in_model.dart';
 import 'package:news_pulse/data/remote/endpoints.dart';
 import '../../core/results/result.dart';
+import '../models/publisher_modell/publisher_modell.dart';
 
 class AuthRemoteDataSourceImp implements AuthRemoteDataSource {
   @override
