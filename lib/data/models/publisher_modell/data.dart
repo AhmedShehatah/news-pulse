da0a3274c7c6ac29397362a09e66027c53a2b517
hussein <husseinpotter19@gmail.com>
2023-08-05 15:23:27 +0300

    feat: Add New Publisher Model

---
+++ lib/data/models/publisher_modell/data.dart
@@ -0,0 +1,19 @@
+class Data {
+	String? userName;
+	String? email;
+	String? id;
+
+	Data({this.userName, this.email, this.id});
+
+	factory Data.fromJson(Map<String, dynamic> json) => Data(
+				userName: json['user_name'] as String?,
+				email: json['email'] as String?,
+				id: json['id'] as String?,
+			);
+
+	Map<String, dynamic> toJson() => {
+				'user_name': userName,
+				'email': email,
+				'id': id,
+			};
+}
