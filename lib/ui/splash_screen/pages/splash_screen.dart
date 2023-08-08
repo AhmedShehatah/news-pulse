85fb4887ddd3749627308fc1a8297662c289837c
Aalaa Adel <aalaaadel02@gmail.com>
2023-08-09 02:04:08 +0300

    feat: splash screen

--- lib/ui/splash_screen/pages/splash_screen.dart
+++ lib/ui/splash_screen/pages/splash_screen.dart
@@ -20,7 +20,7 @@ class _SplashScreenState extends State<SplashScreen> {
   void initState() {
     super.initState();
     Timer(
-        const Duration(seconds: 5),
+        const Duration(seconds: 3),
         () => Navigator.pushReplacement(context,
             MaterialPageRoute(builder: (context) => const HomeParentPage())));
   }
