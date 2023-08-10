07a7c986bd27d66e9cc9f01cccdfb9df34afdb11
hussein <husseinpotter19@gmail.com>
2023-08-10 14:28:30 +0300

    refactor: rename to AddNewsPage

--- lib/core/navigation/route_generator.dart
+++ lib/core/navigation/route_generator.dart
@@ -1,5 +1,6 @@
 import 'package:animations/animations.dart';
 import 'package:flutter/material.dart';
+import 'package:news_pulse/ui/add_news/pages/add_news.dart';
 import 'package:news_pulse/ui/contact_us/pages/contact_us.dart';
 import 'package:news_pulse/ui/home/pages/home_page.dart';
 import 'package:news_pulse/ui/sigin/pages/signin_page.dart';
@@ -41,11 +42,13 @@ class RouteGenerator {
       case HomePage.routeName:
         return const HomePage();
       case ContactUs.routeName:
-        return ContactUs();
+        return const ContactUs();
       case SignInPage.routName:
         return const SignInPage();
       case SignUpPage.routName:
         return const SignUpPage();
+      case AddNewsPage.routName:
+        return const AddNewsPage();
       default:
         settings = RouteSettings(
             arguments: settings.arguments, name: DefaultRoute.routeName);
