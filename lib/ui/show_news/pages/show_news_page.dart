5a41398ab20ade14c59c09934924ce9367653571
Ebtehal Ahmed <tota.mash99@gmail.com>
2023-08-11 01:28:01 +0200

    refactor ui

--- lib/ui/show_news/pages/show_news_page.dart
+++ lib/ui/show_news/pages/show_news_page.dart
@@ -1,15 +1,17 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
-
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:intl/intl.dart';
 import 'package:news_pulse/blocs/news/news_bloc.dart';
+import 'package:news_pulse/core/constants/app_style.dart';
+import 'package:news_pulse/core/constants/dimens.dart';
 import 'package:news_pulse/core/di/di_manager.dart';
 import 'package:news_pulse/core/states/base_fail_state.dart';
 import 'package:news_pulse/core/states/base_success_state.dart';
 import 'package:news_pulse/core/states/base_wait_state.dart';
+import 'package:news_pulse/core/utils/screen_utlis/device_utils.dart';
+import 'package:news_pulse/core/utils/ui_utlis/vertical_padding.dart';
 import 'package:news_pulse/data/models/news_model.dart';
-
 import '../../../blocs/news/news_state.dart';
 
 // ignore: must_be_immutable
@@ -27,6 +29,7 @@ class _ShowNewsPageState extends State<ShowNewsPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: DIManager.findCC().lightgreyBackground,
       body: BlocBuilder<NewsCubit, NewsState>(
         bloc: DIManager.findDep<NewsCubit>(),
         builder: (context, state) {
@@ -45,45 +48,44 @@ class _ShowNewsPageState extends State<ShowNewsPage> {
           } else if (showState is BaseSuccessState) {
             var news = showState.data as NewsModel;
             return SingleChildScrollView(
-              child: Form(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    CachedNetworkImage(imageUrl: news.imageUrl!),
-                    const SizedBox(
-                      height: 10,
-                    ),
-                    Row(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  CachedNetworkImage(
+                    imageUrl: news.imageUrl!,
+                    fit: BoxFit.fill,
+                    width: ScreenHelper.width,
+                  ),
+                  Padding(
+                    padding: Dimens.cardInternalPadding,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        const SizedBox(
-                          width: 10,
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              news.publisher!,
+                              style: AppStyle.text14Style,
+                            ),
+                            Text(
+                                DateFormat("EEEE, MM, dd")
+                                    .format(news.createdAt!),
+                                style: AppStyle.text14Style),
+                          ],
                         ),
+                        const VerticalPadding(1.5),
                         Text(
-                          news.publisher!,
-                          style: const TextStyle(fontSize: 20),
+                          news.title!,
+                          style: AppStyle.bigTitleStyle,
                         ),
-                        const SizedBox(
-                          width: 200,
-                        ),
-                        Text(DateFormat("EEEE, MM, dd").format(news.createdAt!),
-                            style: const TextStyle(fontSize: 20)),
+                        const VerticalPadding(1.5),
+                        Text(news.content!, style: AppStyle.text14Style),
                       ],
                     ),
-                    const SizedBox(
-                      height: 10,
-                    ),
-                    Text(
-                      news.title!,
-                      style: const TextStyle(
-                          fontWeight: FontWeight.bold, fontSize: 25),
-                    ),
-                    const SizedBox(
-                      height: 10,
-                    ),
-                    Text(news.content!, style: const TextStyle(fontSize: 20)),
-                  ],
-                ),
+                  ),
+                ],
               ),
             );
           } else {
