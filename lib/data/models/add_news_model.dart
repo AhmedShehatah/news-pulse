507e359b731dd85bc084c5ead6ae6b20b66a24ee
hussein <husseinpotter19@gmail.com>
2023-08-10 14:29:04 +0300

    feat: Add image url to model

--- lib/data/models/add_news_model.dart
+++ lib/data/models/add_news_model.dart
@@ -6,15 +6,17 @@ class AddNewsModel {
   File? image;
   String? imageUrl;
 
-  AddNewsModel({this.title, this.content});
+  AddNewsModel({this.title, this.content, this.image, this.imageUrl});
 
   factory AddNewsModel.fromJson(Map<String, dynamic> json) => AddNewsModel(
         title: json['title'] as String?,
         content: json['content'] as String?,
+        imageUrl: json['image_url'] as String?,
       );
 
   Map<String, dynamic> toJson() => {
         'title': title,
         'content': content,
+        'image_url': imageUrl,
       };
 }
