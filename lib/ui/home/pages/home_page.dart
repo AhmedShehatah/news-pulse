ab698e7598016445ea96bd40d4eb6705123dfba3
hussein <husseinpotter19@gmail.com>
2023-08-12 10:32:16 +0300

    feat: Add Publisher News View

--- lib/ui/home/pages/home_page.dart
+++ lib/ui/home/pages/home_page.dart
@@ -2,12 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:news_pulse/blocs/news/news_bloc.dart';
 import 'package:news_pulse/core/di/di_manager.dart';
+import 'package:news_pulse/core/shared_prefs/shared_prefs.dart';
 import 'package:news_pulse/core/states/base_success_state.dart';
 import 'package:news_pulse/core/states/base_wait_state.dart';
 import 'package:news_pulse/data/models/news_model.dart';
 import 'package:news_pulse/ui/home/widgets/news_item_model.dart';
 
 import '../../../blocs/news/news_state.dart';
+import '../../publisher_news/publisher_show_news.dart';
 import '../../show_news/pages/show_news_page.dart';
 
 class HomePage extends StatefulWidget {
@@ -19,6 +21,7 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  bool isPublisher = DIManager.findDep<SharedPrefs>().getToken() != null;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -47,8 +50,11 @@ class _HomePageState extends State<HomePage> {
                 return InkWell(
                     onTap: () {
                       DIManager.findNavigator().pushNamed(
-                          ShowNewsPage.routeName,
-                          arguments: newsList[index].id!);
+                        arguments: newsList[index].id!,
+                        isPublisher
+                            ? PublisherShowNewsPage.routeName
+                            : ShowNewsPage.routeName,
+                      );
                     },
                     child: NewsItemModel(model: newsList[index]));
               },
